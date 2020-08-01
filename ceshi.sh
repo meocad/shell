292993a7979cea1e44b2d04a36e6101b1e3bcc7f
meocad <meocadjoke@gmail.com>
2020-08-01 18:29:35 +0800

    shell learning

---
+++ ceshi.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#****************************************************************#
+# ScriptName: ceshi.sh
+# Author: utcmxr@outlook.com
+# Create Date: 2019-12-25 17:36
+# Modify Author: utcmxr@outlook.com
+# Modify Date: 2019-12-25 17:36
+# Function: 
+#***************************************************************#
+
+echo $1 $- $# $@ $! $? $$ "!$" $*
